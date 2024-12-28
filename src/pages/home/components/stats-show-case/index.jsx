d0a47bd9428a078a57d5c958b5c9ce0ccdd0eb98
abdulazizjavascript <abdulazizjavascript@gmail.com>
2024-12-28 16:43:45 +0500

    ins photo

--- src/pages/home/components/stats-show-case/index.jsx
+++ src/pages/home/components/stats-show-case/index.jsx
@@ -1,7 +1,7 @@
 export default function StatsShowCase() {
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 py-10">
       <h2 className="text-2xl font-bold text-center mb-6">
         Sizni nimalar kutyapti
       </h2>
