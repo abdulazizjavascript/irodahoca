e67ed45188ab1a7ff0207d041579a68c195005ce
abdulazizjavascript <abdulazizjavascript@gmail.com>
2025-01-11 22:35:22 +0500

    Added payment bot

--- src/pages/home/components/admin/index.jsx
+++ src/pages/home/components/admin/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 
 import AdminBtn from './../admin-btn/index';
 const Admin = () => {
@@ -5,7 +6,7 @@ const Admin = () => {
     <section className="py-16">
       <div className="container text-center">
         <h1 className="font-bold text-center bg-red-600 py-3 px-3 inline-block text-white text-3xl rounded-xl">SAVOLLARINGIZ BO'LSA SO'RANG</h1>
-        <AdminBtn title="Adminga yozish" />
+        <AdminBtn link="https://t.me/irodahoca_admin" title="Adminga yozish" />
       </div>
     </section>
   )
