1eaed6810d71815753bb2f3e8c67a7eefb71aa5b
abdulazizjavascript <abdulazizjavascript@gmail.com>
2026-05-05 03:57:16 +0500

    Changed home

--- src/pages/home/components/hero/index.jsx
+++ src/pages/home/components/hero/index.jsx
@@ -3,9 +3,8 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useState } from "react";
 
-import irodahoca from "../../../../assets/irodahoca-strelka-2.webp";
+import irodahoca from "../../../../assets/irodahoca-strelka.avif";
 import RegisterModal from "../../../../components/registeration-modal";
-import Gifts from "../../../../components/gifts";
 
 import ReactPixel from "react-facebook-pixel";
 
@@ -15,10 +14,6 @@ export default function Page() {
   return (
     <section className="bg-white roboto-condensed">
       <div className="container pt-4 pb-8 text-center">
-        <h3 className="text-[17.5px] text-yellow-500 font-bold mb-2">
-          Turkcha gaplash va 100% sertifikatli bo'l
-        </h3>
-
         <h1 className="flex justify-center text-[18px] font-bold mb-3 text-white uppercase">
           <span className="px-3 rounded-tl-lg rounded-bl-lg py-1 bg-green-500">
             Turkçe konuş
@@ -27,24 +22,17 @@ export default function Page() {
             100% sertifika
           </span>
         </h1>
+        <h3 className="text-[16px] font-bold mb-2">
+          <span className="text-[17.5px]">KURSIMIZ ORQALI SIZNI 0 DAN 3 OYDA</span>
+          <br />
+          <span>B2 SERTIFIKAT DARAJASIGA CHIQARAMIZ</span>
+          <br />
+          <span className="text-[14.5px]">IMTIHONGA KIRIB SERTIFIKAT OLOLMASANGIZ</span>
+          <br />
+          <span className="text-[21.5px] text-red-600">PULINGIZNI 100% QAYTARAMIZ</span>
+        </h3>
 
-        <div className="max-w-3xl mx-auto mb-8">
-          <h2 className="font-bold mb-4">
-            <span className="text-md">
-              Turk tilini 0 dan 2-3 oyda haftada 7 kun 3 ta
-            </span>{" "}
-            <br />{" "}
-            <span className="text-[16.8px]">
-              ustoz bilan o'qitib sizni sertifikatli qilamiz
-            </span>{" "}
-            <br />{" "}
-            <span className="text-[21.3px] text-red-600">
-              aks holda pulingizni qaytaramiz !
-            </span>
-          </h2>
-        </div>
-
-        <div className="relative max-w-xl mx-auto">
+        <div className="relative max-w-xl mx-auto mb-10">
           <LazyLoadImage
             className="mx-auto mb-8 rounded-3xl w-full object-cover h-[250px]"
             alt="Irodahoca"
@@ -67,15 +55,27 @@ export default function Page() {
               target="_blank"
               onClick={() => ReactPixel.track( "Lead" )}
               rel="noopener noreferrer"
-              className="flex -mt-10 z-10 font-bold bg-red-600 text-white px-8 py-2 rounded-xl text-3xl hover:bg-red-700 transition-colors animate-pulseGlow"
+              className="flex -mt-10 z-10 font-bold bg-red-600 text-white px-8 py-2 rounded-xl text-xl hover:bg-red-700 transition-colors animate-pulseGlow"
             >
-              ADMINGA YOZISH
+              TEZROQ MA'LUMOT OLISH
             </a>
             {/* <p>Sizni sertifikatli qilmaguncha qo'ymaymiz !</p> */}
-            <p className="text-[14px]">Yuqoridagi "Adminga yozish" tugmasini bosing. <br /> Sizga kurs va narxlari haqidagi ma'lumotlar <br /> adminimiz tomonidan yuborib qo'yiladi.</p>
+            {/* <p className="text-[14px]">Yuqoridagi "Adminga yozish" tugmasini bosing. <br /> Sizga kurs va narxlari haqidagi ma'lumotlar <br /> adminimiz tomonidan yuborib qo'yiladi.</p> */}
           </div>
-          <Gifts />
+
         </div>
+
+        <div className="max-w-3xl mx-auto font-bold mb-8">
+          <h2 className="inline-block p-2 rounded-lg bg-red-500 text-white mb-4">
+            FAQAT MAY OYI UCHUN 3+1 CHEGIRMASI
+          </h2>
+          <p>
+            MAY OYIDA 3 OYLIK KURSIMIZGA YOZILSANGIZ
+            <br />
+            <span className="text-red-600">SENTABRDAGI IMTIHONGACHA BEPUL O'QIYSIZ</span>
+          </p>
+        </div>
+
       </div>
       <RegisterModal
         isOpen={isModalOpen}
